142f957033fd6f6268c1aca48445bdfb6b5dee97
alexandre-soares-93 <asoaresdeoliveira977@gmail.com>
2025-05-26 18:54:25 -0300

    Criação das funcoes com a lista de candidatos e gerador randomico de salario

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,11 +1,39 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
 
     }
+
+    static void selecaoCandidatos() {
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela",
+        "Daniela", "Jorge"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário R$" + salarioPretendido);
+
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para vaga");
+                candidatosSelecionados++;
+            }
+
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
 
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
